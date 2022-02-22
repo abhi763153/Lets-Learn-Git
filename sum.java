9efd3aee50eebe793ca4c44367aa6ac491fa11ca
Abhishek Kumar Kamti <abhishekkamti723@gmail.com>
2022-02-22 10:49:13 +0530

    My initial commit

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum{
+
+    public static void main(String [] args){
+
+        int a = 10;
+        int b = 30;
+
+        int c = a+b;
+
+        System.out.println("The sum of a and b is "+c);
+    }
+}
